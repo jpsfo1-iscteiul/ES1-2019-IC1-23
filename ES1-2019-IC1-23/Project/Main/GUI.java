c75e77569f8d51a21385d67bc576a33718724e49
jpsfo1-iscteiul <jpsfo1@iscte-iul.pt>
2019-12-11 16:22:42 +0000

    Clearence

--- ES1-2019-IC1-23/Project/Main/GUI.java
+++ ES1-2019-IC1-23/Project/Main/GUI.java
@@ -1,4 +1,4 @@
-package gui;
+package Main;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
--- ES1-2019-IC1-23/Project/Main/Thresholds.java
+++ ES1-2019-IC1-23/Project/Main/Thresholds.java
@@ -1,4 +1,4 @@
-package gui;
+package Main;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
