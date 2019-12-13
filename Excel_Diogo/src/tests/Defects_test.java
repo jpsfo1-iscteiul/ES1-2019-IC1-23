72f159b455584b50e5bc4fb524769dae52f2e266
rtcze-iscteiul <rtcze@iscte-iul.pt>
2019-12-13 20:01:28 +0000

    acabei os teste

---
+++ Excel_Diogo/src/tests/Defects_test.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Defects_test {
+	Defects d1= new Defects(new JTable j, )
+	@Test
+	public void test() {
+		
+	}
+
+}
